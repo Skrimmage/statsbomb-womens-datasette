af7c9778089a2cd00d9b94d7899e7fd2c421bc67
Danny Page <dannypage@users.noreply.github.com>
2018-08-09 16:22:57 -0700

    Create push.sh

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_database() {
+  git checkout master
+  git add . *.db
+  git commit --message "Automated Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git back_home add origin https://${GH_TOKEN}@github.com/skrimmage/statsbomb-womens-datasette.git > /dev/null 2>&1
+  git push --quiet --set-upstream back_home master
+}
+
+setup_git
+commit_database
+upload_files
