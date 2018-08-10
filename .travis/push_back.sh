ef1a18c847ffa7a1a2f7dd0f55e11f08c89a4889
Danny Page <dannypage@users.noreply.github.com>
2018-08-09 21:01:57 -0700

    Fix Gitignore and Git commands

--- .travis/push_back.sh
+++ .travis/push_back.sh
@@ -12,7 +12,7 @@ commit_database() {
 }
 
 upload_files() {
-  git back_home add origin https://${GH_TOKEN}@github.com/skrimmage/statsbomb-womens-datasette.git > /dev/null 2>&1
+  git remote add back_home https://${GH_TOKEN}@github.com/skrimmage/statsbomb-womens-datasette.git > /dev/null 2>&1
   git push --quiet --set-upstream back_home master
 }
 
